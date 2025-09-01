bcc0418402521ff0e782a61e8a4bb8621f2b47dc
Ajay Mathew <ajay.02mathew@gmail.com>
2025-09-01 22:04:05 +0530

    Update HomePage.jsx

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -4,26 +4,30 @@ import { Header } from '../../components/Header';
 import { ProductsGrid } from './productsGrid';
 import './HomePage.css';
 
-
-export function HomePage({ cart,loadCart }) {
-
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchHomeData = async () => {
-      const response = await axios.get('https://ecommerce-project-for-learning-react.onrender.com/api/products');
-      setProducts(response.data);
-    }
+      try {
+        const response = await axios.get(
+          'https://ecommerce-project-for-learning-react.onrender.com/api/products'
+        );
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
     fetchHomeData();
   }, []);
-
 
   return (
     <>
       <title>Ecommerce Project</title>
       <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products} loadCart={loadCart}/>
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
